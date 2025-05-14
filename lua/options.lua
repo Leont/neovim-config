304065c8da096f68f3559900f71badca0f1f7e34
Leon Timmermans <fawaka@gmail.com>
2025-05-14 12:19:04 +0200

    Set my usual options

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,12 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt = 'both' -- to enable cursorline!
+o.expandtab = false
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+o.ignorecase = false
+o.number = false
+o.whichwrap = '[]hl,b,s'
